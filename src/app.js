30bbde0f50e81c67bc636266396d7d36a2e33a7b
Frahaan Hussain <Sonar Systems>
2014-10-10 08:15:21 +0100

    Using The Accelerometer

---
+++ src/app.js
@@ -0,0 +1,47 @@
+
+var HelloWorldLayer = cc.Layer.extend({
+    sprite:null,
+    ctor:function () {
+        //////////////////////////////
+        // 1. super init first
+        this._super();
+
+        /////////////////////////////
+        // 2. add a menu item with "X" image, which is clicked to quit the program
+        //    you may modify it.
+        // ask the window size
+        var size = cc.winSize;
+
+        var sprite = new cc.Sprite.create(res.CloseNormal_png);
+        sprite.setAnchorPoint(cc.p(0.5, 0.5));
+        sprite.setPosition(cc.p(size.width / 2, size.height / 2));
+        this.addChild(sprite, 0);
+
+        if (cc.sys.capabilities.hasOwnProperty('accelerometer'))
+        {
+        	cc.inputManager.setAccelerometerInterval( 1 / 10 );
+        	cc.inputManager.setAccelerometerEnabled(true);
+
+        	cc.eventManager.addListener(
+        	{
+        		event: cc.EventListener.ACCELERATION,
+
+        		callback: function(accelEvent, event)
+        		{
+        			cc.log('Accel X: ' + accelEvent.x + ' Y: ' + accelEvent.y + ' Z: ' + accelEvent.z);
+        		}
+        	}, this);
+        }
+
+        return true;
+    }
+});
+
+var HelloWorldScene = cc.Scene.extend({
+    onEnter:function () {
+        this._super();
+        var layer = new HelloWorldLayer();
+        this.addChild(layer);
+    }
+});
+
